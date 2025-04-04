9ced89431fc8848981852af7091c3c0b753ab444
Tamim Islam <nottamimislam@gmail.com>
2025-04-04 18:52:12 +0600

    O

--- house_project/urls.py
+++ house_project/urls.py
@@ -18,7 +18,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
-    path('api/', include('service.urls')),
+    path('', include('service.urls')),
     path('api/app', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
