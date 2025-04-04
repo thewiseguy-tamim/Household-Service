e4ee54a5ec0d646cea12e455a8731ed28a1c0b38
Tamim Islam <nottamimislam@gmail.com>
2025-04-04 18:58:36 +0600

    Final

--- house_project/urls.py
+++ house_project/urls.py
@@ -17,7 +17,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('users/', include('users.urls')),
     path('', include('service.urls')),
     path('api/app', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
